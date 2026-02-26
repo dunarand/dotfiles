61c70b00a21e0c3bf11f481703ce61e90672bb3a
dunarand <dunarand@protonmail.com>
2026-02-26 07:39:02 +0300

    dropped vscode support
    I'll probably implement it later

--- vscode-extensions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -f "$HOME/dotfiles/config/Code/User/extensions.txt" ]; then
-    cat "$HOME/dotfiles/config/Code/User/extensions.txt" | xargs -L 1 code --install-extension
-    echo "VS Code extensions installed"
-else
-    echo "extensions.txt not found"
-fi
