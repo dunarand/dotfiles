0c08064ef61b09000a30ef60ac570bd59215b8f7
dunarand <dunarand@protonmail.com>
2026-02-27 15:11:12 +0300

    nvim themery removed unused themes

--- config/nvim/lua/plugins/themery.lua
+++ config/nvim/lua/plugins/themery.lua
@@ -2,9 +2,6 @@ return {
 	"zaldih/themery.nvim",
 	dependencies = {
 		"catppuccin/nvim",
-		"olimorris/onedarkpro.nvim",
-		"navarasu/onedark.nvim",
-		"rose-pine/neovim",
 	},
 	lazy = false,
 	config = function()
@@ -22,67 +19,6 @@ return {
 					colorscheme = "catppuccin-mocha",
 					before = [[
                         vim.opt.background = "dark"
-                    ]],
-				},
-				{
-					name = "onedarkpro-vaporwave",
-					colorscheme = "onedark_vivid",
-					before = [[
-                        vim.opt.background = "dark"
-                        require('onedarkpro').setup({
-                            options = {
-                                transparency = false,
-                            }
-                        })
-                    ]],
-				},
-				{
-					name = "onedarkpro-dark",
-					colorscheme = "onedark_dark",
-					before = [[
-                        vim.opt.background = 'dark'
-                    ]],
-				},
-				{
-					name = "onedark-darker",
-					colorscheme = "onedark",
-					before = [[
-                        vim.opt.background = 'dark'
-                        require('onedark').setup({
-                            style = 'darker'
-                        })
-                        require('onedark').load()
-                    ]],
-				},
-				{
-					name = "onedark-deep",
-					colorscheme = "onedark",
-					before = [[
-                        vim.opt.background = 'dark'
-                        require('onedark').setup({
-                            style = 'deep'
-                        })
-                        require('onedark').load()
-                    ]],
-				},
-				{
-					name = "rose-pine",
-					colorscheme = "rose-pine",
-					before = [[
-                        vim.opt.background = 'dark'
-                        require('rose-pine').setup({
-                            variant = "main"
-                        })
-                    ]],
-				},
-				{
-					name = "rose-pine-dawn",
-					colorscheme = "rose-pine-dawn",
-					before = [[
-                        vim.opt.background = 'light'
-                        require('rose-pine').setup({
-                            variant = "dawn"
-                        })
                     ]],
 				},
 			},
