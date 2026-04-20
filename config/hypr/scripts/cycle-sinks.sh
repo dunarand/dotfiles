e0e29d130d8e13b45597174cc9688e4d8905a8d3
dunarand <dunarand@protonmail.com>
2026-04-20 07:59:31 +0300

    audio output switch improved notification

--- config/hypr/scripts/cycle-sinks.sh
+++ config/hypr/scripts/cycle-sinks.sh
@@ -22,3 +22,12 @@ pactl set-default-sink "$new_sink"
 # Move all streams to the new sink
 pactl list short sink-inputs | awk '{print $1}' \
     | xargs -I{} pactl move-sink-input {} "$new_sink"
+
+desc=$(pactl list sinks | awk -v sink="$new_sink" '
+$0 ~ "Name: "sink {found=1}
+found && /Description:/ {sub("Description: ", ""); print; exit}
+')
+
+notify-send -h string:x-canonical-private-synchronous:audio-switch \
+            -h int:transient:1 \
+            "Audio Output Switched: $desc"
