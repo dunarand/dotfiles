46dddc4c63b93a77b442c99d1ba1387f8d5bfd6d
dunarand <dunarand@protonmail.com>
2026-05-03 15:20:36 +0300

    dropped old minimize script

--- config/hypr/scripts/minimize.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -z $(hyprctl workspaces | grep special:magic) ]]; then
-    hyprctl dispatch movetoworkspacesilent special:magic
-else
-    hyprctl --batch 'dispatch togglespecialworkspace magic;dispatch movetoworkspace +0'
-fi
